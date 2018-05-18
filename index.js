d51d050ad2eca22a005bd68045fd84b71b7738b6
m2wasabi <m2wasabi@gmail.com>
2018-05-18 11:40:57 +0900

    First app

---
+++ index.js
@@ -0,0 +1,21 @@
+var env = require('node-env-file');
+env(__dirname + '/.env');
+
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot({
+    debug: true,
+    // clientId: process.env.clientId,
+    // clientSecret: process.env.clientSecret,
+    // clientVerificationToken: process.env.clientVerificationToken,
+    json_file_store: __dirname + '/.data/db/',
+    scopes: ['bot']
+});
+
+var bot = controller.spawn({
+    token: process.env.oauthToken
+}).startRTM();
+
+controller.hears('hello','direct_message', function(bot, message) {
+    bot.reply(message,'Hello yourself!');
+});
